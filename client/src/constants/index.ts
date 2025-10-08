3da70a77452f65d6cdf5cb7bbae91c3c2158267f
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2025-10-08 10:21:17 +0530

    Update BottomNavigator

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,6 +1,13 @@
 export const ROUTES = {
   ROOT: { MAIN: "Main", MODAL: "Modal" },
-  TABS: { HOME: "Home", BOOKINGS: "Bookings", SETTINGS: "Settings" , PRICEFORECAST: "Price Forecast"},
+  TABS: { 
+    HOME: "Home", 
+    PRICEFORECAST: "PriceForecast",
+    PESTIDENTIFIER: "PestIdentifier",
+    DISEASEIDENTIFIER: "DiseaseIdentifier",
+    FERTILIZERADVISOR: "FertilizerAdvisor",
+    USERPROFILE: "UserProfile",
+  },
 } as const;
 
 export const API_BASE = "http://192.168.1.50:8000"; // change to your Python server (FastAPI/Django/Flask)
