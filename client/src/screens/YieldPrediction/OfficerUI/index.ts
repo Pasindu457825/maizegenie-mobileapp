be11656f58c3e5ea63e7a5235236c7a3b3505b9b
Sithum Suraweera <it22339492@my.sliit.lk>
2025-12-08 20:46:18 +0530

    remove old frontend files yield pred

--- client/src/screens/YieldPrediction/OfficerUI/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * AgriOfficer UI Screens Export
- */
-
-export { SoilProfileScreen } from './SoilProfileScreen';
-export { ClimateScreen } from './ClimateScreen';
-export { CropMeasurementsScreen } from './CropMeasurementsScreen';
-export { FertilizerSchedulingScreen } from './FertilizerSchedulingScreen';
