9d7ae3d07074d080880ed0963f3ddedda1dccfe0
Pasindu Rasanjana <153684733+Pasindu457825@users.noreply.github.com>
2025-12-30 22:05:00 +0530

    සඳහා

--- client/src/data/diseases/treatments/commonRustTreatments.ts
+++ client/src/data/diseases/treatments/commonRustTreatments.ts
@@ -127,7 +127,7 @@ export const commonRustTreatments: SriLankanTreatment[] = [
     type: "chemical",
     name: {
       en: "Tebuconazole 250 EC (Severe infection)",
-      si: "ටෙබුකොනසෝල් 250 EC (දැඩි ආසාදනය)",
+      si: "ටෙබුකොනසෝල් 250 EC (ඉහළ ආසාදන සඳහා)",
     },
     availableProducts: {
       en: ["Folicur 250 EW", "Orius"],
